df6f73806c48675639a0c9c03b4d34875194f019
marielalescano <marielalescano31@gmail.com>
2022-06-16 18:51:33 -0300

    función bienvenida

---
+++ funciones/bienvenida.py
@@ -0,0 +1,11 @@
+
+def bienvenida():
+    print("*" * 50)
+    print("*"," "*46,"*")
+    print("*          Bienvenidos al repositorio            *")
+    print("*             del Grupo 2 Aula 7                 *")
+    print("*"," "*46,"*")
+    print("*" * 50)
+
+
+bienvenida()
