1b14a8f07e774f99ba4419860c12d953c9683776
marielalescano <marielalescano31@gmail.com>
2022-07-06 13:59:02 -0300

    #11 suma del producto más el tercer parámetro

---
+++ funciones/funcionp1.py
@@ -0,0 +1,11 @@
+from producto import producto
+
+
+def funcionp1(a,b):
+   c=a+b
+   return c
+
+a=producto(2,4)
+d=funcionp1(a,2)
+
+print("La suma del producto de los dos primeros parámetros más el tercer parámetro es",d)
